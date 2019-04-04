a3cd6e83764d0fc37f9569eedf257b30415f4320
evolyuta <dima.97_97@mail.ru>
2019-04-04 17:09:37 +0300

    found hopt

--- Definite_integral_composite.py
+++ Definite_integral_composite.py
@@ -5,21 +5,22 @@ import numpy as np
 
 # real answer = 4.46151
 
-maxn = 13
+maxn = 4
 ans = []
 coefAns = []
-maxk = 40
-durAns = []  # answer for each section
+maxk = 51
+durAns = []
 
-for n in range(2, maxn):
+for n in range(3, maxn):
 
     print('\n\n\n\nn=', n)
 
-    for currMaxk in range(39, maxk):
+    for currMaxk in range(1, maxk):
 
         print('k=', currMaxk)
         durAns = []
         h = 1.2 / currMaxk
+        print(' h =', h)
 
         for k in range(0, currMaxk):
 
@@ -31,13 +32,10 @@ for n in range(2, maxn):
             pointMatrix = []
             strpointMatrix = []
             currAns = 0
-            absSumCoef = 0
             duration = lastPoint - firstPoint
 
             for i in range(n):
                 point.append(firstPoint + i * duration / (n - 1))
-
-            # print(point)
 
             f = lambda t: (4.5 * np.cos(7 * (t + 2.1)) * np.exp(-2 * (t + 2.1) / 3)
                            + 1.4 * np.sin(1.5 * (t + 2.1)) * np.exp(-(t + 2.1) / 3) + 3)
@@ -59,46 +57,114 @@ for n in range(2, maxn):
             for i in range(n):
                 currAns = currAns + coefs[i] * f(point[i])
 
-            for i in range(n):
-                absSumCoef = absSumCoef + abs(coefs[i])
-
-            coefAns.append(absSumCoef)
-            # print(' abs coefficient sum=', absSumCoef)
             durAns.append(currAns)
 
-        # print(durAns)
-        sumdurAns = 0
+        h1Ans = 0
         for i in range(len(durAns)):
-            sumdurAns = sumdurAns + durAns[i]
-        print(' quadrature sum=', sumdurAns)
-        ans.append(sumdurAns)
+            h1Ans = h1Ans + durAns[i]
+        print(' Sh1 =', h1Ans)
 
-print(len(ans))
+        h2 = h / 2
+        currMaxk = currMaxk * 2
+        durAns = []
 
+        for k in range(0, currMaxk):
 
+            firstPoint = k * h2
+            lastPoint = (k + 1) * h2
 
-args = range(2, maxn)
-ords = ans
+            fi = []
+            point = []
+            pointMatrix = []
+            strpointMatrix = []
+            currAns = 0
+            duration = lastPoint - firstPoint
 
-plt.plot(args, ords, linewidth=2)
-plt.ylabel('Quadrature sum')
+            for i in range(n):
+                point.append(firstPoint + i * duration / (n - 1))
 
-for i_x, i_y in zip(args, ords):
-    if i_x % 1 == 0:
-        plt.text(i_x, i_y, '({}, {})'.format(i_x, '%.2f' % i_y))
+            f = lambda t: (4.5 * np.cos(7 * (t + 2.1)) * np.exp(-2 * (t + 2.1) / 3)
+                           + 1.4 * np.sin(1.5 * (t + 2.1)) * np.exp(-(t + 2.1) / 3) + 3)
+            p = lambda t: (t ** -0.4)
 
-# args1 = range(2, maxn)
-# ords1 = coefAns
-#
-# plt.plot(args1, ords1, linewidth=2)
-# plt.ylabel('blue Quad sum, yellow abs coef sum')
-#
-# for i_x, i_y in zip(args1, ords1):
-#     if i_x % 1 == 0:
-#         plt.text(i_x, i_y, '({}, {})'.format(i_x, '%.2f' % i_y))
+            for i in range(n):
+                fi.append(lastPoint ** (i + 3 / 5) / (i + 3 / 5) - firstPoint ** (i + 3 / 5) / (i + 3 / 5))
 
-mng = plt.get_current_fig_manager()
-mng.resize(*mng.window.maxsize())
+            for i in range(n):
+                for j in range(n):
+                    strpointMatrix.append(point[j] ** i)
+                pointMatrix.append(strpointMatrix)
+                strpointMatrix = []
 
-plt.show()
+            pointMatrix = np.linalg.inv(pointMatrix)
 
+            coefs = np.dot(pointMatrix, fi)
+
+            for i in range(n):
+                currAns = currAns + coefs[i] * f(point[i])
+
+            durAns.append(currAns)
+
+        h2Ans = 0
+        for i in range(len(durAns)):
+            h2Ans = h2Ans + durAns[i]
+        print(' Sh2 =', h2Ans)
+        h3 = h / 4
+        currMaxk = currMaxk * 2
+        durAns = []
+
+        for k in range(0, currMaxk):
+
+            firstPoint = k * h3
+            lastPoint = (k + 1) * h3
+
+            fi = []
+            point = []
+            pointMatrix = []
+            strpointMatrix = []
+            currAns = 0
+            duration = lastPoint - firstPoint
+
+            for i in range(n):
+                point.append(firstPoint + i * duration / (n - 1))
+
+            f = lambda t: (4.5 * np.cos(7 * (t + 2.1)) * np.exp(-2 * (t + 2.1) / 3)
+                           + 1.4 * np.sin(1.5 * (t + 2.1)) * np.exp(-(t + 2.1) / 3) + 3)
+            p = lambda t: (t ** -0.4)
+
+            for i in range(n):
+                fi.append(lastPoint ** (i + 3 / 5) / (i + 3 / 5) - firstPoint ** (i + 3 / 5) / (i + 3 / 5))
+
+            for i in range(n):
+                for j in range(n):
+                    strpointMatrix.append(point[j] ** i)
+                pointMatrix.append(strpointMatrix)
+                strpointMatrix = []
+
+            pointMatrix = np.linalg.inv(pointMatrix)
+
+            coefs = np.dot(pointMatrix, fi)
+
+            for i in range(n):
+                currAns = currAns + coefs[i] * f(point[i])
+
+            durAns.append(currAns)
+
+        h3Ans = 0
+        for i in range(len(durAns)):
+            h3Ans = h3Ans + durAns[i]
+        print(' Sh3 =', h3Ans)
+
+        m = -  np.log((h3Ans - h2Ans) / (h2Ans - h1Ans)) / np.log(2)
+        eps = 0.000001
+        Rh = h1Ans + (h2Ans - h1Ans) / (1 - np.power(2, -m)) - h1Ans
+        print(' Rh = ', Rh)
+        hopt = h * np.power((eps * (1 - np.power(2, -m)) / abs(h2Ans - h1Ans)), 1 / m)
+        print(' hopt = ', hopt)
+        print(' m = ', m)
+
+hopt = hopt * 0.95
+print("\n\n\n\n\n\n\n\n\n\n\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n\nhopt=",
+      hopt)
+maxk = int(1.2 / hopt + 0.5)
+print('k = ', maxk)
