713d3b4df4972d11bbdc305a7414e37ea026b1e2
evolyuta <dima.97_97@mail.ru>
2019-04-06 00:41:16 +0300

    plot eps to hopt

--- Definite_integral_Gauss_2.py
+++ Definite_integral_Gauss_2.py
@@ -9,61 +9,47 @@ x = Symbol('x')
 
 # real answer = 4.46151
 
-maxn = 3
-ans = []
-coefAns = []
+n = 2
 
-for n in range(2, maxn):
+print('n=', n)
 
-    print('n=', n)
+fi = []
+fins = []
+fijs = []
+amatrix = []
+point = []
 
-    fi = []
-    point = []
-    pointmatrix = []
-    strpointmatrix = []
-    currAns = 0
-    absSumCoef = 0
-    fins = []
-    fijs = []
-    amatrix = []
+f = lambda t: (4.5 * np.cos(7.0 * (t + 2.1)) * np.exp(-2.0 * (t + 2.1) / 3.0)
+               + 1.4 * np.sin(1.5 * (t + 2.1)) * np.exp(-(t + 2.1) / 3.0) + 3.0)
+p = lambda t: (t ** -0.4)
 
-    # for i in range(n):
-    #     point.append(i * 1.2 / (n - 1))
+for i in range(2 * n):
+    fi.append(1.2 ** (i + 3 / 5) / (i + 3 / 5))
 
-    f = lambda t: (4.5 * np.cos(7.0 * (t + 2.1)) * np.exp(-2.0 * (t + 2.1) / 3.0)
-                   + 1.4 * np.sin(1.5 * (t + 2.1)) * np.exp(-(t + 2.1) / 3.0) + 3.0)
-    p = lambda t: (t ** -0.4)
+for s in range(0, n):
+    fins.append(-fi[n + s])
+    dsf = []
+    for j in range(0, n):
+        dsf.append(fi[j + s])
+    fijs.append(dsf)
 
-    for i in range(2 * n):
-        fi.append(1.2 ** (i + 3 / 5) / (i + 3 / 5))
+amatrix.append((fi[0] * fi[3] - fi[2] * fi[1]) / (fi[1] ** 2 - fi[0] * fi[2]))
+amatrix.append((fi[2] * fi[2] - fi[3] * fi[1]) / (fi[1] ** 2 - fi[0] * fi[2]))
 
-    for s in range(0, n):
-        fins.append(-fi[n + s])
-        dsf = []
-        for j in range(0, n):
-            dsf.append(fi[j + s])
-        fijs.append(dsf)
+discr = amatrix[0] * amatrix[0] - 4 * amatrix[1]
+point.append((-amatrix[0] - np.sqrt(discr)) / 2)
+point.append((-amatrix[0] + np.sqrt(discr)) / 2)
 
-    amatrix.append((fi[0] * fi[3] - fi[2] * fi[1]) / (fi[1] ** 2 - fi[0] * fi[2]))
-    amatrix.append((fi[2] * fi[2] - fi[3] * fi[1]) / (fi[1] ** 2 - fi[0] * fi[2]))
+print('points=', point)
 
-    eq = amatrix[0]
-    for i in range(1, maxn - 1):
-        eq = x ** i * amatrix[i] + eq
-    eq = x ** n + eq
+pointmatrix = [[1, 1], [point[0], point[1]]]
 
-    point = solve(eq)
+A2 = (fi[1] - fi[0] * point[0]) / (-point[0] + point[1])
+A1 = fi[0] - A2
+print('coefs=', A1, A2)
 
-    print('points=', point)
+point[0] = float(point[0])
+point[1] = float(point[1])
 
-    pointmatrix = [[1, 1], [point[0], point[1]]]
-
-    A2 = (fi[1] - fi[0] * point[0]) / (-point[0] + point[1])
-    A1 = fi[0] - A2
-    print('coefs=',A1, A2)
-
-    point[0] = float(point[0])
-    point[1] = float(point[1])
-
-    res = A1 * f(point[0]) + A2 * f(point[1])
-    print('result=',res)
+res = A1 * f(point[0]) + A2 * f(point[1])
+print('result=', res)
--- Definite_integral_composite.py
+++ Definite_integral_composite.py
@@ -1,170 +1,203 @@
 import matplotlib.pyplot as plt
-
-plt.rcdefaults()
 import numpy as np
 
 # real answer = 4.46151
 
-maxn = 4
+maxn = 3
 ans = []
 coefAns = []
 maxk = 51
 durAns = []
+currMaxk = 5
+Rh = 1
 
-for n in range(3, maxn):
+epsArray = []
+hoptArray = []
 
-    print('\n\n\n\nn=', n)
+eps = 1.0e-5
 
-    for currMaxk in range(1, maxk):
+while (eps>1.0e-8):
 
-        print('k=', currMaxk)
-        durAns = []
-        h = 1.2 / currMaxk
-        print(' h =', h)
+    print('eps=',eps)
 
-        for k in range(0, currMaxk):
+    for n in range(maxn, maxn+1):
 
-            firstPoint = k * h
-            lastPoint = (k + 1) * h
+        # print('\n\n\n\nn=', n)
 
-            fi = []
-            point = []
-            pointMatrix = []
-            strpointMatrix = []
-            currAns = 0
-            duration = lastPoint - firstPoint
+        while (abs(Rh) > eps):
 
-            for i in range(n):
-                point.append(firstPoint + i * duration / (n - 1))
+            # for currMaxk in range(1, maxk):
 
-            f = lambda t: (4.5 * np.cos(7 * (t + 2.1)) * np.exp(-2 * (t + 2.1) / 3)
-                           + 1.4 * np.sin(1.5 * (t + 2.1)) * np.exp(-(t + 2.1) / 3) + 3)
-            p = lambda t: (t ** -0.4)
+            maxk = currMaxk
 
-            for i in range(n):
-                fi.append(lastPoint ** (i + 3 / 5) / (i + 3 / 5) - firstPoint ** (i + 3 / 5) / (i + 3 / 5))
+            print('k=', currMaxk)
+            durAns = []
+            h = 1.2 / currMaxk
+            # print(' h =', h)
 
-            for i in range(n):
-                for j in range(n):
-                    strpointMatrix.append(point[j] ** i)
-                pointMatrix.append(strpointMatrix)
+            for k in range(0, currMaxk):
+
+                firstPoint = k * h
+                lastPoint = (k + 1) * h
+
+                fi = []
+                point = []
+                pointMatrix = []
                 strpointMatrix = []
+                currAns = 0
+                duration = lastPoint - firstPoint
 
-            pointMatrix = np.linalg.inv(pointMatrix)
+                for i in range(n):
+                    point.append(firstPoint + i * duration / (n - 1))
 
-            coefs = np.dot(pointMatrix, fi)
+                f = lambda t: (4.5 * np.cos(7 * (t + 2.1)) * np.exp(-2 * (t + 2.1) / 3)
+                               + 1.4 * np.sin(1.5 * (t + 2.1)) * np.exp(-(t + 2.1) / 3) + 3)
+                p = lambda t: (t ** -0.4)
 
-            for i in range(n):
-                currAns = currAns + coefs[i] * f(point[i])
+                for i in range(n):
+                    fi.append(lastPoint ** (i + 3 / 5) / (i + 3 / 5) - firstPoint ** (i + 3 / 5) / (i + 3 / 5))
 
-            durAns.append(currAns)
+                for i in range(n):
+                    for j in range(n):
+                        strpointMatrix.append(point[j] ** i)
+                    pointMatrix.append(strpointMatrix)
+                    strpointMatrix = []
 
-        h1Ans = 0
-        for i in range(len(durAns)):
-            h1Ans = h1Ans + durAns[i]
-        print(' Sh1 =', h1Ans)
+                pointMatrix = np.linalg.inv(pointMatrix)
 
-        h2 = h / 2
-        currMaxk = currMaxk * 2
-        durAns = []
+                coefs = np.dot(pointMatrix, fi)
 
-        for k in range(0, currMaxk):
+                for i in range(n):
+                    currAns = currAns + coefs[i] * f(point[i])
 
-            firstPoint = k * h2
-            lastPoint = (k + 1) * h2
+                durAns.append(currAns)
 
-            fi = []
-            point = []
-            pointMatrix = []
-            strpointMatrix = []
-            currAns = 0
-            duration = lastPoint - firstPoint
+            h1Ans = 0
+            for i in range(len(durAns)):
+                h1Ans = h1Ans + durAns[i]
+            # print(' Sh1 =', h1Ans)
 
-            for i in range(n):
-                point.append(firstPoint + i * duration / (n - 1))
+            h2 = h / 2
+            currMaxk = currMaxk * 2
+            durAns = []
 
-            f = lambda t: (4.5 * np.cos(7 * (t + 2.1)) * np.exp(-2 * (t + 2.1) / 3)
-                           + 1.4 * np.sin(1.5 * (t + 2.1)) * np.exp(-(t + 2.1) / 3) + 3)
-            p = lambda t: (t ** -0.4)
+            for k in range(0, currMaxk):
 
-            for i in range(n):
-                fi.append(lastPoint ** (i + 3 / 5) / (i + 3 / 5) - firstPoint ** (i + 3 / 5) / (i + 3 / 5))
+                firstPoint = k * h2
+                lastPoint = (k + 1) * h2
 
-            for i in range(n):
-                for j in range(n):
-                    strpointMatrix.append(point[j] ** i)
-                pointMatrix.append(strpointMatrix)
+                fi = []
+                point = []
+                pointMatrix = []
                 strpointMatrix = []
+                currAns = 0
+                duration = lastPoint - firstPoint
 
-            pointMatrix = np.linalg.inv(pointMatrix)
+                for i in range(n):
+                    point.append(firstPoint + i * duration / (n - 1))
 
-            coefs = np.dot(pointMatrix, fi)
+                f = lambda t: (4.5 * np.cos(7 * (t + 2.1)) * np.exp(-2 * (t + 2.1) / 3)
+                               + 1.4 * np.sin(1.5 * (t + 2.1)) * np.exp(-(t + 2.1) / 3) + 3)
+                p = lambda t: (t ** -0.4)
 
-            for i in range(n):
-                currAns = currAns + coefs[i] * f(point[i])
+                for i in range(n):
+                    fi.append(lastPoint ** (i + 3 / 5) / (i + 3 / 5) - firstPoint ** (i + 3 / 5) / (i + 3 / 5))
 
-            durAns.append(currAns)
+                for i in range(n):
+                    for j in range(n):
+                        strpointMatrix.append(point[j] ** i)
+                    pointMatrix.append(strpointMatrix)
+                    strpointMatrix = []
 
-        h2Ans = 0
-        for i in range(len(durAns)):
-            h2Ans = h2Ans + durAns[i]
-        print(' Sh2 =', h2Ans)
-        h3 = h / 4
-        currMaxk = currMaxk * 2
-        durAns = []
+                pointMatrix = np.linalg.inv(pointMatrix)
 
-        for k in range(0, currMaxk):
+                coefs = np.dot(pointMatrix, fi)
 
-            firstPoint = k * h3
-            lastPoint = (k + 1) * h3
+                for i in range(n):
+                    currAns = currAns + coefs[i] * f(point[i])
 
-            fi = []
-            point = []
-            pointMatrix = []
-            strpointMatrix = []
-            currAns = 0
-            duration = lastPoint - firstPoint
+                durAns.append(currAns)
 
-            for i in range(n):
-                point.append(firstPoint + i * duration / (n - 1))
+            h2Ans = 0
+            for i in range(len(durAns)):
+                h2Ans = h2Ans + durAns[i]
+            # print(' Sh2 =', h2Ans)
+            h3 = h / 4
+            currMaxk = currMaxk * 2
+            durAns = []
 
-            f = lambda t: (4.5 * np.cos(7 * (t + 2.1)) * np.exp(-2 * (t + 2.1) / 3)
-                           + 1.4 * np.sin(1.5 * (t + 2.1)) * np.exp(-(t + 2.1) / 3) + 3)
-            p = lambda t: (t ** -0.4)
+            for k in range(0, currMaxk):
 
-            for i in range(n):
-                fi.append(lastPoint ** (i + 3 / 5) / (i + 3 / 5) - firstPoint ** (i + 3 / 5) / (i + 3 / 5))
+                firstPoint = k * h3
+                lastPoint = (k + 1) * h3
 
-            for i in range(n):
-                for j in range(n):
-                    strpointMatrix.append(point[j] ** i)
-                pointMatrix.append(strpointMatrix)
+                fi = []
+                point = []
+                pointMatrix = []
                 strpointMatrix = []
+                currAns = 0
+                duration = lastPoint - firstPoint
 
-            pointMatrix = np.linalg.inv(pointMatrix)
+                for i in range(n):
+                    point.append(firstPoint + i * duration / (n - 1))
 
-            coefs = np.dot(pointMatrix, fi)
+                f = lambda t: (4.5 * np.cos(7 * (t + 2.1)) * np.exp(-2 * (t + 2.1) / 3)
+                               + 1.4 * np.sin(1.5 * (t + 2.1)) * np.exp(-(t + 2.1) / 3) + 3)
+                p = lambda t: (t ** -0.4)
 
-            for i in range(n):
-                currAns = currAns + coefs[i] * f(point[i])
+                for i in range(n):
+                    fi.append(lastPoint ** (i + 3 / 5) / (i + 3 / 5) - firstPoint ** (i + 3 / 5) / (i + 3 / 5))
 
-            durAns.append(currAns)
+                for i in range(n):
+                    for j in range(n):
+                        strpointMatrix.append(point[j] ** i)
+                    pointMatrix.append(strpointMatrix)
+                    strpointMatrix = []
 
-        h3Ans = 0
-        for i in range(len(durAns)):
-            h3Ans = h3Ans + durAns[i]
-        print(' Sh3 =', h3Ans)
+                pointMatrix = np.linalg.inv(pointMatrix)
 
-        m = -  np.log((h3Ans - h2Ans) / (h2Ans - h1Ans)) / np.log(2)
-        eps = 0.000001
-        Rh = h1Ans + (h2Ans - h1Ans) / (1 - np.power(2, -m)) - h1Ans
-        print(' Rh = ', Rh)
-        hopt = h * np.power((eps * (1 - np.power(2, -m)) / abs(h2Ans - h1Ans)), 1 / m)
-        print(' hopt = ', hopt)
-        print(' m = ', m)
+                coefs = np.dot(pointMatrix, fi)
 
-hopt = hopt * 0.95
-print("\n\n\n\n\n\n\n\n\n\n\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n\nhopt=",
-      hopt)
-maxk = int(1.2 / hopt + 0.5)
-print('k = ', maxk)
+                for i in range(n):
+                    currAns = currAns + coefs[i] * f(point[i])
+
+                durAns.append(currAns)
+
+            h3Ans = 0
+            for i in range(len(durAns)):
+                h3Ans = h3Ans + durAns[i]
+            # print(' Sh3 =', h3Ans)
+
+            m = -  np.log((h3Ans - h2Ans) / (h2Ans - h1Ans)) / np.log(2)
+            Rh = h1Ans + (h2Ans - h1Ans) / (1 - np.power(2, -m)) - h1Ans
+            # print(' Rh = ', Rh)
+            hopt = h * np.power((eps * (1 - np.power(2, -m)) / abs(h2Ans - h1Ans)), 1 / m)
+            # print(' hopt = ', hopt)
+            # print(' m = ', m)
+            currMaxk = int(1.2 / hopt + 1.5)#maxk + 1
+
+    # hopt = hopt * 0.95
+    print("\n\n\n\n\n\n\n\n\n\n\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n\nhopt=",
+          hopt)
+    maxk = int(1.2 / hopt + 0.5)
+    epsArray.append(eps)
+    hoptArray.append(hopt)
+    eps=eps/1.5
+
+
+# print(epsArray,hopt)
+
+args = range(6,17)
+ords = hoptArray[6:17]
+
+plt.plot(args, ords, linewidth=2)
+plt.ylabel('Quadrature sum')
+
+for i_x, i_y in zip(args, ords):
+    if i_x % 1 == 0:
+        plt.text(i_x, i_y, '({}, {})'.format(i_x, '%.5f' % i_y))
+
+mng = plt.get_current_fig_manager()
+mng.resize(*mng.window.maxsize())
+
+plt.show()
